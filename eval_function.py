228217a314134c1cc01dfe813d49f0967f194b39
Manoj Titarmare <manojtitarmare8@gmail.com>
2019-08-24 16:19:14 +0530

    Initial commit

---
+++ eval_function.py
@@ -0,0 +1,17 @@
+#eval() function is used to automatically convert to the Datatype
+# when user enter the input fromkeyboard
+#eval() also evaluates expressions and return result
+
+"""
+Date: 24-08-2019 , time: 15:34 written by Manoj Titarmare
+"""
+
+x = eval(input('enter something :'))
+print(x,type(x))
+
+y = eval('10+30+45')
+print(y,type(y))
+
+
+
+
