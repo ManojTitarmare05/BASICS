a80432ebc3fae0ec3ebf3f3f2d0b02777194d24b
Manoj Titarmare <manojtitarmare8@gmail.com>
2019-08-27 22:59:33 +0530

    startswith() and endswith

---
+++ string_2.py
@@ -0,0 +1,8 @@
+# example for startswith() and endswith() functions
+# startswith(substring) ----check the string
+
+s = 'Happy to explain about python string'
+print(s.startswith('Happy'))
+print(s.endswith('string'))
+
+
